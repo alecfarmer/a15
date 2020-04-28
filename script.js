ca7e782cf015301f5e1d5db43a8915515b8c9fa4
Alec <47801939+alecfarmer@users.noreply.github.com>
2020-04-28 02:29:00 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,295 @@
+// James Alec Farmer
+window.onload = function() {
+    this.displayPlayers();
+
+    // ADD BUTTON POP OUT
+    let addExeBtn = document.getElementById("adding-pop");
+    let buttonA = this.document.getElementById("btn-add-pop");
+    let btn = this.document.getElementById("btn-add-player");
+    // DISPLAY POP OUT
+    buttonA.onclick = function() {
+        addExeBtn.style.display = "block";
+    }
+    // ADD PLAYER / CLOSE POP OUT
+    btn.onclick = function() {
+        addPlayer();
+        setTimeout(() => { addExeBtn.style.display = "none"; }, 800);
+    }
+    // CLOSE POP OUT
+    window.onclick = function(event) {
+        if(event.target == addExeBtn) {
+            addExeBtn.style.display = "none";
+        }
+    }
+    // END ADD POP OUT
+
+
+    // START EDIT BUTTON POP OUT
+    let editExeBtn = document.getElementById("edit-pop");
+    /// --> Button found in js creation below \\\
+    let btnEdit = document.getElementById("btn-edit-player");
+    // EDIT PLAYER / CLOSE POP OUT
+    btnEdit.onclick = function() {
+        editPlayer();
+        setTimeout(() => { editExeBtn.style.display = "none"; }, 800);
+    }
+    // CLOSE POP OUT
+    window.onclick = function(event) {
+        if(event.target == editExeBtn) {
+            editExeBtn.style.display = "none";
+        }
+    }
+    // END EDIT POP OUT
+
+
+    // START DELETE POP OUT
+    let deleteExeBtn = document.getElementById("delete-pop");
+    /// --> Button found in js creation below \\\
+    let btnDelete = document.getElementById("btn-delete-player");
+    let btnNevermind = document.getElementById("btn-exit");
+    btnDelete.onclick = function() {
+        deletePlayer();
+        setTimeout(() => { deleteExeBtn.style.display = "none"; }, 800);
+    }
+    // NEVERMIND CLOSE
+    btnNevermind.onclick = function() {
+        deleteExeBtn.style.display = "none";
+    }
+    // CLOSE POP OUT
+    window.onclick = function(event) {
+        if(event.target == deleteExeBtn) {
+            deleteExeBtn.style.display = "none";
+        }
+    }
+    // END DELETE POP OUT
+
+}
+async function displayPlayers() {
+    let response = await fetch('api/players/');
+    let playersJSON = await response.json();
+    let playersDiv = document.getElementById("player-list");
+    playersDiv.innerHTML = "";
+
+    for(i in playersJSON) {
+        let player = playersJSON[i];
+        playersDiv.append(getPlayerInfo(player));
+    }
+}
+
+function getPlayerInfo(player) {
+
+    // MAIN DIV
+    let mainDiv = document.createElement("div");
+    mainDiv.classList.add("player");
+
+    // INFO SECTION
+    let infoSection = document.createElement("section");
+    mainDiv.append(infoSection);
+
+    // BUTTON SECTION
+    let buttonSection = document.createElement("Section");
+    buttonSection.classList.add("buttonSec");
+    mainDiv.append(buttonSection);
+
+    // TITLE
+    let title = document.createElement("h2");
+    title.textContent = `${player.name} - ${player.commitment}`;
+    title.classList.add("playertexth2");
+    infoSection.append(title);
+
+    // REVIEWER
+    let reviewer = document.createElement("h5");
+    reviewer.textContent = `By ${player.reviewer}`;
+    reviewer.classList.add("playertexth4");
+    infoSection.append(reviewer);
+
+    // EDIT BUTTON
+    let buttonEdit = document.createElement("button");
+    buttonEdit.innerHTML = `Edit`;
+    buttonEdit.classList.add("innerButtons");
+    let editExeBtn = document.getElementById("edit-pop");
+    buttonEdit.onclick = function() {
+        showPlayerDetails(player);
+        editExeBtn.style.display = "block";
+    }
+    buttonSection.append(buttonEdit);
+
+    // DELETE BUTTON
+    let buttonDelete = document.createElement("button");
+    buttonDelete.innerHTML = `Delete`;
+    buttonDelete.classList.add("innerButtons");
+    let deleteExeBtn = document.getElementById("delete-pop");
+    buttonDelete.onclick = function() {
+        showPlayerDetails(player);
+        deleteExeBtn.style.display = "block";
+    }
+    buttonSection.append(buttonDelete);
+
+    return mainDiv;
+}
+
+async function showPlayerDetails(player) {
+    let playerId = player._id
+    let response = await fetch(`/api/players/${playerId}`);
+
+    if(response.status != 200) {
+        // DISPLAY ERROR
+        console.log("Error reciving player!");
+        return;
+    }
+
+    player = await response.json();
+    document.getElementById("player-id").textContent = playerId;
+    document.getElementById("txt-name").value = player.name;
+    document.getElementById("txt-age").value = player.age;
+    document.getElementById("txt-position").value = player.position;
+    document.getElementById("txt-bt").value = player.batThrow;
+    document.getElementById("txt-commit").value = player.commitment;
+    document.getElementById("txt-gradyear").value = player.gradYear;
+    document.getElementById("txt-natrank").value = player.nationRanking;
+    document.getElementById("txt-staterank").value = player.stateRanking;
+    document.getElementById("txt-rev").value = player.reviewer;
+}
+
+async function addPlayer() {
+    let playerName = document.getElementById("txt-add-name").value;
+    let playerAge = document.getElementById("txt-add-age").value;
+    let playerPosition = document.getElementById("txt-add-position").value;
+    let playerBatThrow = document.getElementById("txt-add-bt").value;
+    let playerCommitment = document.getElementById("txt-add-commit").value;
+    let playerGradYear = document.getElementById("txt-add-gradyear").value;
+    let playerNatRank = document.getElementById("txt-add-natrank").value;
+    let playerStateRank = document.getElementById("txt-add-staterank").value;
+    let playerRev = document.getElementById("txt-add-rev").value;
+
+    let player = {"name":playerName, "age":playerAge, "position":playerPosition, "batThrow":playerBatThrow, "commitment":playerCommitment,
+        "gradYear":playerGradYear, "nationRanking":playerNatRank, "stateRanking":playerStateRank, "reviewer":playerRev};
+    
+    let response = await fetch('/api/players', {
+        method:"POST",
+        headers:{
+            'Content-Type':'application/json;charset=utf-8',
+        },
+        body:JSON.stringify(player),
+    });
+
+    if(response.status != 200) {
+        let span = document.getElementById("notifyType");
+        let note = document.getElementById("note");
+        note.classList.toggle("active");
+        span.classList.toggle("failure");
+
+        setTimeout(function(){
+            note.classList.remove("active");
+            span.classList.remove("failure");
+        }, 3000);
+        return;
+    } 
+    else {
+        let span = document.getElementById("notifyType");
+        let note = document.getElementById("note");
+        note.classList.toggle("active");
+        span.classList.toggle("success");
+
+        setTimeout(function() {
+            note.classList.remove("active");
+            span.classList.remove("success");
+        }, 3000);
+    }
+
+    let result = await response.json();
+    console.log(result);
+    displayPlayers();
+}
+
+async function editPlayer() {
+    let playerId = document.getElementById("player-id").textContent;
+    console.log(playerId);
+    let playerName = document.getElementById("txt-name").value;
+    let playerAge = document.getElementById("txt-age").value;
+    let playerPosition = document.getElementById("txt-position").value;
+    let playerBatThrow = document.getElementById("txt-bt").value;
+    let playerCommitment = document.getElementById("txt-commit").value;
+    let playerGradYear = document.getElementById("txt-gradyear").value;
+    let playerNatRank = document.getElementById("txt-natrank").value;
+    let playerStateRank = document.getElementById("txt-staterank").value;
+    let playerRev = document.getElementById("txt-rev").value;
+
+    let player = {"name":playerName, "age":playerAge, "position":playerPosition, "batThrow":playerBatThrow, "commitment":playerCommitment,
+        "gradYear":playerGradYear, "nationRanking":playerNatRank, "stateRanking":playerStateRank, "reviewer":playerRev};
+
+    let response = await fetch(`/api/players/${playerId}`, {
+        method:'PUT',
+        headers:{
+            'Content-Type':'application/json;charset=utf-8',
+        },
+        body: JSON.stringify(player)
+    });
+
+    if(response.status != 200) {
+        let span = document.getElementById("notifyType");
+        let note = document.getElementById("note");
+        note.classList.toggle("active");
+        span.classList.toggle("failure");
+
+        setTimeout(function(){
+            note.classList.remove("active");
+            span.classList.remove("failure");
+        }, 3000);
+        return;
+    } 
+    else {
+        let span = document.getElementById("notifyType");
+        let note = document.getElementById("note");
+        note.classList.toggle("active");
+        span.classList.toggle("success");
+
+        setTimeout(function() {
+            note.classList.remove("active");
+            span.classList.remove("success");
+        }, 3000);
+    }
+
+    let result = await response.json();
+    console.log(result);
+    displayPlayers();
+}
+
+async function deletePlayer(){
+    let playerId = document.getElementById("player-id").textContent;
+    
+    let response = await fetch(`/api/players/${playerId}`,{
+        method:"DELETE",
+        headers:{
+            'Content-Type':'application/json;charset=utf-8',
+        }
+    });
+
+    if(response.status != 200) {
+        let span = document.getElementById("notifyType");
+        let note = document.getElementById("note");
+        note.classList.toggle("active");
+        span.classList.toggle("failure");
+
+        setTimeout(function(){
+            note.classList.remove("active");
+            span.classList.remove("failure");
+        }, 3000);
+        return;
+    } 
+    else {
+        let span = document.getElementById("notifyType");
+        let note = document.getElementById("note");
+        note.classList.toggle("active");
+        span.classList.toggle("success");
+
+        setTimeout(function() {
+            note.classList.remove("active");
+            span.classList.remove("success");
+        }, 3000);
+    }
+
+    let result = await response.json();
+    console.log(result);
+    displayPlayers();
+}
---
+++ server.js
@@ -0,0 +1,139 @@
+// James Alec Farmer
+const express = require("express");
+const app = express();
+const Joi = require("joi");
+app.use(express.static("public"));
+app.use(express.json());
+const mongoose = require("mongoose");
+
+// CONNECT TO DATABASE
+mongoose.connect("mongodb+srv://alec:test@fpcluster-ovts8.mongodb.net/test?retryWrites=true&w=majority", {useUnifiedTopology:true, useNewUrlParser:true})
+    .then(()=>console.log("Successfully connected to mongo database!"))
+    .catch(err => console.error("Couldn't connect to mongo database!", err));
+
+// DATABASE LAYOUT
+const playerSchema = new mongoose.Schema ({
+    name:String,
+    age:Number,
+    position:String,
+    batThrow:String,
+    commitment:String,
+    gradYear:String,
+    nationRanking:Number,
+    stateRanking:Number,
+    reviewer:String
+});
+
+const Player = mongoose.model('Player', playerSchema);
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get('/api/players', (req, res)=>{
+    getPlayers(res);
+});
+
+async function getPlayers(res) {
+    const players = await Player.find();
+    console.log(players);
+    res.send(players);
+}
+
+app.get('/api/players/:id', (req,res)=>{
+    getPlayer(req.params.id, res);
+});
+
+async function getPlayer(id, res) {
+    const player = await Player.findOne({_id:id});
+    console.log(player);
+    res.send(player);
+}
+
+app.post('/api/players', (req, res)=>{
+    const result = validatePlayer(req.body);
+
+    if(result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    const player = new Player({
+        name:req.body.name,
+        age:req.body.age,
+        position:req.body.position,
+        batThrow:req.body.batThrow,
+        commitment:req.body.commitment,
+        gradYear:req.body.gradYear,
+        nationRanking:req.body.nationRanking,
+        stateRanking:req.body.stateRanking,
+        reviewer:req.body.reviewer
+    });
+
+    createPlayer(player, res);
+});
+
+async function createPlayer(player, res) {
+    const result = await player.save();
+    console.log(result);
+    res.send(player);
+}
+
+app.put('/api/players/:id', (req, res)=>{
+    const result = validatePlayer(req.body);
+
+    if(result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    updatePlayer(res, req.params.id, req.body.name, req.body.age, req.body.position, req.body.batThrow, req.body.commitment,
+        req.body.gradYear, req.body.nationRanking, req.body.stateRanking, req.body.reviewer);
+});
+
+async function updatePlayer(res, id, name, age, position, batThrow, commitment, gradYear, nationRanking, stateRanking, reviewer) {
+    const result = await Player.updateOne({_id:id}, {
+        $set:{
+            name:name,
+            age:age,
+            position:position,
+            batThrow:batThrow,
+            commitment:commitment,
+            gradYear:gradYear,
+            nationRanking:nationRanking,
+            stateRanking:stateRanking,
+            reviewer:reviewer
+        }
+    })
+
+    res.send(result);
+}
+
+app.delete('/api/players/:id', (req, res)=>{
+    removePlayer(res, req.params.id);
+});
+
+async function removePlayer(res, id) {
+    const player = await Player.findByIdAndRemove(id);
+    res.send(player);
+}
+
+function validatePlayer(player) {
+    const schema = {
+        name:Joi.string().min(3).required(),
+        age:Joi.number().min(1).required(),
+        position:Joi.string().min(3).required(),
+        batThrow:Joi.string().min(2).required(),
+        commitment:Joi.string().min(3).required(),
+        gradYear:Joi.number().min(2).required(),
+        nationRanking:Joi.number().min(1).required(),
+        stateRanking:Joi.number().min(1).required(),
+        reviewer:Joi.string().min(3).required()
+    };
+
+    return Joi.validate(player, schema);
+}
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log("Listening on port 3000");
+})
